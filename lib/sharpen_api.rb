ba9a5f80305c0cf31bb3de621a28f51976cc9a32
StoveSolder <ssadler@instructure.com>
2020-08-06 14:27:39 -0600

    update to agent_data method and helper module

--- lib/sharpen_api.rb
+++ lib/sharpen_api.rb
@@ -4,7 +4,7 @@ require 'json'
 
 ##
 # Sharpen APIs for sending sms text messages
-class SharpenApi
+module SharpenApi
   include HTTParty
   base_uri 'https://api.fathomvoice.com/'
 
@@ -24,14 +24,18 @@ class SharpenApi
       'q' => "SELECT (CASE WHEN `event` = 'queue login' THEN `eventTime` END) AS \"Queue Login Event Time\", (CASE WHEN `event` = 'interaction' THEN `eventTime` END) AS \"Total Interactions Event Time\", (CASE WHEN `event` = 'pause' THEN `eventTime` END) AS \"Pause Event Time\", (CASE WHEN `event` = 'interaction' AND `eventActionStart` = 'caseIn' THEN `eventTime` END) AS \"Case In Interaction Event Time\", `queueADRID`, `context`, `username`, `agentName`, `userGroupName`, `userGroupID`, `queueName`, `startTime`, `endTime`, `eventTime`, `event`, `eventDetails`, `queueCallManagerID`, `eventActionStart`, `eventActionEnd` FROM `fathomvoice`.`fathomQueues`.`queueADR` WHERE `endTime` >= \"#{startTime.strftime.gsub(/T/,' ').gsub(/-06:00/,"")}\" AND `endTime` <= \"#{endTime.strftime.gsub(/T/,' ').gsub(/-06:00/,"")}\" LIMIT 20000"
     }
 
-    response = SharpenApi.post(url, headers: headers, body: options.to_json)
+    response = JSON.parse(SharpenApi.post(url, headers: headers, body: options.to_json))
     return response
   end
 
-  def self.agent_data_csv(json_response)
-    response = JSON.parse(json_response)
-    csv_string = CSV.generate do |csv|
-      response['data'].each do |entry|
-        csv << entry[:username], entry[:event], entry[:startTime]+'.000'
+ def self.agent_data_csv(json_response)
+      csv_string = CSV.generate do |csv|
+      json_response['data'].each do |entry|
+          csv << entry[:userName]
+          csv << entry[:event]
+          csv << entry[:startTime]+'.000'
+        end
+      end
     return csv_string
+   end
 end
--- lib/tasks/agent_availability.rake
+++ lib/tasks/agent_availability.rake
@@ -2,5 +2,5 @@
 desc 'gather agent historic availability data'
 task agent_availability: :environment do
   response = SharpenApi.agent_data(DateTime.now, DateTime.now - 15.minutes)
-  agent_data_csv(response)
+  SharpenApi.agent_data_csv(response)
 end
