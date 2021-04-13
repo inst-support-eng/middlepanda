526043cc889cba30c76e2a421b4ff3d40e864923
StoveSolder <ssadler@instructure.com>
2021-04-13 11:48:40 -0600

    changed variable, increased interval to 5

--- lib/clockwork.rb
+++ lib/clockwork.rb
@@ -8,6 +8,6 @@ module Clockwork
         puts "Running #{job}"
       end
 
-      every(ENV['AGENT_SYNC_INTERVAL'].day, 'AgentsAddWorker.perform_async', :at => '00:00') {AgentsAddWorker.perform_async}
-      every(ENV['STATUS_UPDATE_INTERVAL'].seconds, 'AgentsStatusWorker.perform_async') {AgentsStatusWorker.perform_async}
+      every(1.day, 'AgentsAddWorker.perform_async', :at => '00:00') {AgentsAddWorker.perform_async}
+      every(5.seconds, 'AgentsStatusWorker.perform_async') {AgentsStatusWorker.perform_async}
 end
